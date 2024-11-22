dfd29fd7dc1cb3669ed07b9ae98ab11927278afe
Juli-ui <57135670+Juli-ui@users.noreply.github.com>
2024-11-22 02:54:36 -0300

    UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+    public TextMeshProUGUI Monedas;
+    public TextMeshProUGUI Life;
+    public TextMeshProUGUI Almas;
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
