0e734a09b7c9872facd575fc489bbe90c907c591
MANUEL OLGUIN <olguin.manuel@maimonidesvirtual.com.ar>
2024-11-23 05:52:20 -0300

    a

---
+++ Assets/EmpezarPelea.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmpezarPelea : MonoBehaviour
+{
+
+    private int playerLayer;
+    private int minibossLayer;
+    private UIManager _uiManager;
+
+    void Start()
+    {
+        // Get the layer indices for the player and miniboss layers
+        playerLayer = LayerMask.NameToLayer("Player");
+        minibossLayer = LayerMask.NameToLayer("MiniBoss");
+        _uiManager = UIManager.Instance;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == playerLayer || other.gameObject.layer == minibossLayer)
+        {
+            _uiManager.showBossFightUI("Miniboss");
+        }
+    }
+}
