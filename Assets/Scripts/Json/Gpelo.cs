ee77b3d98c7833c10095a3e11488d9159f28526d
Juli-ui <57135670+Juli-ui@users.noreply.github.com>
2024-11-23 01:26:07 -0300

    Gpelo

--- Assets/Scripts/Json/Gpelo.cs
+++ Assets/Scripts/Json/Gpelo.cs
@@ -8,4 +8,17 @@ public class Gpelo : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab; 
     [SerializeField] private Transform shootPoint; 
     [SerializeField] private float shootCooldown = 2f;
+    public Player player;
+    [SerializeField] int bulletLayer;
+    [SerializeField] Animator zombieController;
+    Rigidbody rb;
+    [Header("Stats")]
+    public float speed = 3f;
+    public float detectionRange = 10f;
+    public float distanceToHit = 0.5f;
+    public float life = 5;
+    public bool isDead = false;
+    float distanceToPlayer;
+
+    private NavMeshAgent navMeshAgent;
 }
