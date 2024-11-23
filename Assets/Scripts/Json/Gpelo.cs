ec25150ac9a1c060802e15628e0a244db6d0fb23
Juli-ui <57135670+Juli-ui@users.noreply.github.com>
2024-11-23 01:39:29 -0300

    Pelota

--- Assets/Scripts/Json/Gpelo.cs
+++ Assets/Scripts/Json/Gpelo.cs
@@ -8,6 +8,8 @@ public class Gpelo : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab; 
     [SerializeField] private Transform shootPoint; 
     [SerializeField] private float shootCooldown = 2f;
+    private float lastShootTime;
+
     public Player player;
     [SerializeField] int bulletLayer;
     [SerializeField] Animator zombieController;
@@ -21,4 +23,130 @@ public class Gpelo : MonoBehaviour
     float distanceToPlayer;
 
     private NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        player = GameManager.Instance.player;
+        rb = GetComponent<Rigidbody>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= detectionRange && !isDead)
+        {
+            // Mueve al enemigo hacia el jugador si está dentro del rango de detección
+            MoveTowardsPlayer();
+        }
+        else
+        {
+            zombieController.SetBool("IsRunning", false);
+            //navMeshAgent.isStopped = true;
+        }
+
+        if (distanceToPlayer <= detectionRange && !isDead)
+        {
+            LookAtPlayer();
+
+            if (Time.time >= lastShootTime + shootCooldown)
+            {
+                ShootProjectile();
+                lastShootTime = Time.time;
+            }
+        }
+    }
+    void MoveTowardsPlayer()
+    {
+        zombieController.SetBool("IsRunning", true);
+        //Vector3 targetPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        //transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        //Vector3 direction = (player.transform.position - transform.position).normalized;
+        //Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        //transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        //navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(player.transform.position);
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        if (distanceToPlayer < distanceToHit)
+        {
+            navMeshAgent.isStopped = true;
+            Attack();
+        }
+        if (distanceToPlayer > distanceToHit)
+            FinishAttack();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<Bullet>(out Bullet bullet))
+            GetHit(bullet.damage);
+    }
+    public void GetHit(float damage)
+    {
+        life -= 1;
+        if (life <= 0)
+            Die();
+    }
+    public void Die()
+    {
+        if (isDead) return;
+        //POner animacion y hacer corrutina para que se destruya despues de la animacion
+        zombieController.SetBool("IsRunning", false);
+        zombieController.SetTrigger("Die");
+        speed = 0;
+
+        isDead = true;
+
+        GetComponent<Collider>().enabled = false;
+
+
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+            rb.velocity = Vector3.zero;
+        }
+        navMeshAgent.isStopped = true;
+        StartCoroutine(DestroyDead());
+    }
+    public IEnumerator DestroyDead()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+    public void Attack()
+    {
+        rb.velocity = Vector3.zero;
+        speed = 0;
+        navMeshAgent.isStopped = true;
+        zombieController.SetBool("IsRunning", false);
+        zombieController.SetTrigger("Attack");
+
+    }
+    public void FinishAttack()
+    {
+        speed = 3;
+        navMeshAgent.isStopped = false;
+    }
+
+    private void LookAtPlayer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void ShootProjectile()
+    {
+        if (projectilePrefab != null && shootPoint != null)
+        {
+            GameObject projectile = Instantiate(projectilePrefab, shootPoint.position, shootPoint.rotation);
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                Vector3 direction = (player.transform.position - shootPoint.position).normalized;
+                rb.velocity = direction * 10f; // Ajusta la velocidad según lo necesites
+            }
+        }
+    }
 }
